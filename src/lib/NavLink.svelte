3ed33c47866741fc8492dc1b673cefdb0673d27b
Nova Mondal <rialmondal@gmail.com>
2023-07-20 17:48:16 -0700

    navbar

---
+++ src/lib/NavLink.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let href: string;
+    export let accent = false;
+</script>
+
+<a {href} class={`p-4 w-fit rounded-md ${accent? "bg-rose-400" : "bg-zinc-700"}`}>
+    <slot/>
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     import '../app.css'
+	import Navbar from '$lib/Navbar.svelte';
 </script>
 
+<Navbar/>
 <slot/>
